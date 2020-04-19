310ef139a5a1f8e7de9c84ea3b9585b88e38e166
paras55 <52483274+paras55@users.noreply.github.com>
2020-04-20 01:50:13 +0530

    Update ocr2.py

--- ocr2.py
+++ ocr2.py
@@ -2,29 +2,35 @@ from PIL import Image
 import pytesseract 
 from pdf2image import convert_from_path 
 import os 
-
+import tempfile
  
-PDF_file = "py.pdf"
-
-pages = convert_from_path(PDF_file, 500) 
-image_counter = 0
-outfile = "out1_text.txt"
+PDF_file = "python.pdf"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+#pages = convert_from_path(PDF_file, 500) 
 
 
-f = open(outfile, "a", encoding="utf-8") 
 
-for page in pages: 	
-    filename = "page_"+str(image_counter)+".jpg"	
-    page.save(filename, 'JPEG') 	
-    image_counter = image_counter + 1
-    text = str(((pytesseract.image_to_string(Image.open(filename))))) 
-
-	
-    text = text.replace('-\n', '')	 
-
-	 
-    f.write(text) 
-    os.unlink(filename)
-
-
-f.close()
+path=r'C:\Users\dell\Desktop\Scripts\thapar\research_electron-master\web_app\flask_app_latest'
+with tempfile.TemporaryDirectory() as path:
+    pages= convert_from_path(PDF_file)
+    image_counter = 0
+    outfile = "out1_text.txt"
+    
+    
+    f = open(outfile, "a", encoding="utf-8") 
+    
+    for page in pages: 	
+        filename = "page_"+str(image_counter)+".jpg"	
+        page.save(filename, 'JPEG') 	
+        image_counter = image_counter + 1
+        text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+    
+    	
+        text = text.replace('-\n', '')	 
+    
+    	 
+        f.write(text) 
+        os.unlink(filename)
+    
+    
+    f.close()

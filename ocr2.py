6066fb3dbf744feff31f4ec49c152b7fbce1cde2
paras55 <52483274+paras55@users.noreply.github.com>
2020-04-19 16:52:19 +0530

    Add files via upload

---
+++ ocr2.py
@@ -0,0 +1,30 @@
+from PIL import Image 
+import pytesseract 
+from pdf2image import convert_from_path 
+import os 
+
+ 
+PDF_file = "table.pdf"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+pages = convert_from_path(PDF_file, 500) 
+image_counter = 0
+outfile = "out1_text.txt"
+
+
+f = open(outfile, "a", encoding="utf-8") 
+
+for page in pages: 	
+    filename = "page_"+str(image_counter)+".jpg"	
+    page.save(filename, 'JPEG') 	
+    image_counter = image_counter + 1
+    text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+
+	
+    text = text.replace('-\n', '')	 
+
+	 
+    f.write(text) 
+    os.unlink(filename)
+
+
+f.close()
